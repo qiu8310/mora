d34a4b34bc3c6d3e67bff5a1a6982a7ec1d878e9
Tony Han <h.bing612@gmail.com>
2015-01-26 20:44:21 +0800

    add fields for system message

--- app/scripts/controllers/home/system.js
+++ app/scripts/controllers/home/system.js
@@ -7,7 +7,9 @@ angular.module('moraApp')
         ios: true,
         android: true
       },
-      date: new Date()
+      date: new Date(),
+      push: true,
+      test: true
     };
 
 
@@ -18,7 +20,12 @@ angular.module('moraApp')
           content: m.content,
           message_type: m.type,
           platforms: _.filter(_.keys(m.platforms), function(k) { return m.platforms[k]; }),
-          schedule: Math.round(m.date.getTime() / 1000)
+          schedule: Math.round(m.date.getTime() / 1000),
+          push: m.push,
+          event_url: m.event_url,
+          event_details: m.event_details,
+          event_cover_img: m.event_cover_img,
+          test: m.test
         }).success(function(data) {
           if (data.success === 'ok') {
             Dialog.alert('提交成功');
